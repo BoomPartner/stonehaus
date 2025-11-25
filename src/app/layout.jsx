d6cb8ccbe4ba05121af812c47156aa98d8e4f24a
BoomPartner <soporte@boom-partner.com>
2025-11-25 09:09:22 -0600

    Ajuste de etique para ADS
    Creacion de etiqueta meta de google ads

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -65,13 +65,31 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KRH9BDR');`}
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KRH9BDR');`}
         </Script>
         {/* End Google Tag Manager */}
+
+        {/* Google tag (gtag.js) - Google Ads */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11049180638"
+          strategy="afterInteractive"
+        />
+
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-11049180638');
+        `}
+        </Script>
+        {/* End Google Ads */}
       </head>
+
       <body className={inter.className}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
